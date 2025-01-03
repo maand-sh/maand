a8512913fb091bb649a116ff3e4d8460e0d8150c
kirubasankars <kiruba.sankars@gmail.com>
2025-01-03 12:30:25 -0500

    harderning rsync

--- src/rsync_upload.sh
+++ src/rsync_upload.sh
@@ -9,19 +9,26 @@ fi
 RULE_FILE=$1
 
 RSYNC_OPTIONS=" \
-  -p -g -o \
-  --ignore-times \
-  --verbose \
-  --force \
-  --delete \
-  --compress \
-  --checksum \
-  --recursive \
-  --exclude=\"jobs/*/bin\" \
+  --timeout 30    \
+  --inplace       \
+  --whole-file    \
+  --checksum      \
+  --recursive     \
+  --force         \
+  --delete-after  \
+  --delete        \
+  --exclude=\"jobs/*/bin\"  \
   --exclude=\"jobs/*/data\" \
   --exclude=\"jobs/*/logs\" \
   --filter='merge /tmp/$RULE_FILE.txt' \
+  --group         \
+  --owner         \
+  --executability \
+  --compress      \
 "
 
+echo
+echo "updating file for $AGENT_IP"
+
 rsync_command="rsync -v --rsync-path=\"$RSYNC_PATH\" $RSYNC_OPTIONS --rsh=\"ssh -o BatchMode=true -o ConnectTimeout=10 -i /bucket/$SSH_KEY\" $AGENT_DIR/ $SSH_USER@$AGENT_IP:/opt/agent/$BUCKET"
-bash -c "$rsync_command" > /dev/null
+bash -c "$rsync_command"> /dev/null
